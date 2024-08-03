0c088c841a9ab9565e90bc4a2f5950273ef6c558
Fajar Adie Santosa <94214658+fnvrxx@users.noreply.github.com>
2024-08-03 23:52:05 +0700

    using c++ 11

--- C++ Class Templates.cpp
+++ C++ Class Templates.cpp
@@ -3,44 +3,47 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cassert>
 using namespace std;
 
+/*Write the class AddElements here*/
 template <typename T>
-class mytemplate{
+class AddElements{
     T elemen1;
     public:
-    mytemplate(T angka) : elemen1(angka){}
-    T add (T elemen2){
+    AddElements(T angka) : elemen1(angka){}
+    T add(T elemen2){
+        return elemen1 + elemen2;
+    }
+    T concatenate(T elemen2){
         return elemen1 + elemen2;
     }
 };
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int input;//banyak input
-    cin >> input;
-    for(int i = 0; i <= input; i++ ){
-        string type;
-        cin >> type;
-        
-        if(type == "string"){
-            string el1,el2;
-            cin >> el1 >> el2;
-            mytemplate<string>mt1(el1);
-            cout << mt1.add(el2) << "\n";
-        }
-        else if (type == "int"){
-            int el1,el2;
-            cin >> el1 >> el2;
-            mytemplate<int>mt2(el1);
-            cout << mt2.add(el2) << "\n";
-        }
-        else if (type == "float"){
-            float el1,el2;
-            cin >> el1 >> el2;
-            mytemplate<float>mt3(el1);
-            cout << mt3.add(el2);
-        }
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
     }
-    return 0;
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
 }
